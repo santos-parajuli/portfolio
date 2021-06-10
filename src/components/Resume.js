e83b9b23a3d946f5673e79aa789cc55c0a1fde92
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2021-06-10 20:53:57 +0545

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -363,6 +363,29 @@ export default function Resume() {
                     <Grid container direction="row">
                         <Grid item xs={12} sm={6} md={4}>
                             <Card className={classes.card}>
+                                <CardMedia component="img" alt="project" image="https://i.imgur.com/xulCSoN.gifv" height="200px" style={{objectFit:"contain"}}/>
+                                <CardContent style={{paddingBottom:"5px"}}>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Santosh Sakar Pustak Pasal Website
+                                    </Typography>
+                                    <Divider/>
+                                    <Typography variant="body2" component="p">
+                                            It is an MERN full stack app for my home shop Santosh Sakar Pustak Pasal. I created this website so that our customers can order the book online.
+                                    </Typography>
+                                    <Divider/>
+                                    <CardActions disableSpacing>
+                                        
+                                        <a href="https://santoshsakar.netlify.app/">
+                                            <Button size="small">
+                                                View Live
+                                            </Button>
+                                        </a>
+                                    </CardActions>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Card className={classes.card}>
                                 <CardMedia component="img" alt="project" image="https://i.imgur.com/NMkzZy5.gif" height="200px" style={{objectFit:"contain"}}/>
                                 <CardContent style={{paddingBottom:"5px"}}>
                                     <Typography gutterBottom variant="h5" component="h2">
@@ -463,29 +486,7 @@ export default function Resume() {
                                     </CardActions>
                                 </CardContent>
                             </Card>
-                            <Grid item xs={12} sm={6} md={4}>
-                            <Card className={classes.card}>
-                                <CardMedia component="img" alt="project" image="https://i.imgur.com/dkbrU9T.gif" height="200px" style={{objectFit:"contain"}}/>
-                                <CardContent style={{paddingBottom:"5px"}}>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        Santosh Sakar Pustak Pasal Website
-                                    </Typography>
-                                    <Divider/>
-                                    <Typography variant="body2" component="p">
-                                            It is an MERN full stack app for my home shop Santosh Sakar Pustak Pasal. I created this website so that our customers can order the book online.
-                                    </Typography>
-                                    <Divider/>
-                                    <CardActions disableSpacing>
-                                        
-                                        <a href="https://santoshsakar.netlify.app/">
-                                            <Button size="small">
-                                                View Live
-                                            </Button>
-                                        </a>
-                                    </CardActions>
-                                </CardContent>
-                            </Card>
-                        </Grid>
+                            
                         </Grid>
                     </Grid>
             </Grid>
