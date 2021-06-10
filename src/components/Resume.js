4fceb2e78534a6cc3f7e901bdf57a21db8479b62
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2021-06-10 20:39:41 +0545

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -463,6 +463,29 @@ export default function Resume() {
                                     </CardActions>
                                 </CardContent>
                             </Card>
+                            <Grid item xs={12} sm={6} md={4}>
+                            <Card className={classes.card}>
+                                <CardMedia component="img" alt="project" image="https://i.imgur.com/dkbrU9T.gif" height="200px" style={{objectFit:"contain"}}/>
+                                <CardContent style={{paddingBottom:"5px"}}>
+                                    <Typography gutterBottom variant="h5" component="h2">
+                                        Santosh Sakar Pustak Pasal Website
+                                    </Typography>
+                                    <Divider/>
+                                    <Typography variant="body2" component="p">
+                                            It is an MERN full stack app for my home shop Santosh Sakar Pustak Pasal. I created this website so that our customers can order the book online.
+                                    </Typography>
+                                    <Divider/>
+                                    <CardActions disableSpacing>
+                                        
+                                        <a href="https://santoshsakar.netlify.app/">
+                                            <Button size="small">
+                                                View Live
+                                            </Button>
+                                        </a>
+                                    </CardActions>
+                                </CardContent>
+                            </Card>
+                        </Grid>
                         </Grid>
                     </Grid>
             </Grid>
